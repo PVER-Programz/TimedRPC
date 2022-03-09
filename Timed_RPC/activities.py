17431800753e7b42419684a0b79da88635954c95
PVER Programz <91049313+PVER-Programz@users.noreply.github.com>
2022-03-09 23:43:00 +0530

    Added Data Files

---
+++ Timed_RPC/activities.py
@@ -0,0 +1,43 @@
+import time
+
+eat = {"small_image":None, "large_image":"bean", "details":"Eating",
+	"state":"brb","large_text":None,"small_text":None , "party_size":None,
+	"buttons":None}
+
+bot = {"small_image":"st3", "large_image":"bot", "details":"Upgrading my bot",
+	"state":"On Sublime","large_text":'Sooper Dooper Bot',"small_text":'Sublime Txt3' , "party_size":None,
+	"buttons":[{"label": "Add Bot", "url": "https://discord.com/api/oauth2/authorize?client_id=872466409240801341&permissions=549755813887&scope=bot"}]}
+
+mfs = {"small_image":None, "large_image":"dscrd", "details":"Riling up mfs",
+	"state":"on random servers lol","large_text":'On discord',"small_text":None , "party_size":[3, 9999],
+	"buttons":None}
+
+# search = {"small_image":"st3", "large_image":"search", "details":"Seaching for that old peice of crappy code",
+# 	"state":"i forgor 💀","large_text":'search in progress',"small_text":'Sublime' , "start":None,
+# 	 "party_size":None,
+# 	"buttons":None}
+
+stare = {"small_image":None, "large_image":"stare", "details":"STARING INTO YOUR SOUL",
+	"state":"yeah i see you !!","large_text":None,"small_text":None , "start":None,
+	 "end":time.time()+1000, "party_size":[2,2],
+	"buttons":None}
+
+afk = {"small_image":None, "large_image":"afk", "details":"I'm AFK",
+	"state":"Don't Ping me","large_text":'Dont Just Dont',"small_text":None , "party_size":None,
+	"buttons":[{"label": "Meanwhile watch my ruined yt", "url": "https://www.youtube.com/c/ClusterSeries"}]}
+
+dnd = {"small_image":'dnddot', "large_image":"dnd", "details":"DONOT DISTURB",
+	"state":"i mean it","large_text":None,"small_text":'DnD' , "party_size":None,
+	"buttons":[{"label": "Need anything ask my secratary", "url": "https://discord.com/channels/@me/872473872342085643"}]}
+
+sleep = {"small_image":None, "large_image":"sleep", "details":"Imma sleep",
+	"state":"im tired","large_text":'zzzzzz',"small_text":None , "party_size":None,
+	"buttons":None}
+
+redt = {"small_image":None, "large_image":"redit", "details":"Scrolling reddit",
+	"state":None,"large_text":'reddit',"small_text":None , "party_size":None,
+	"buttons":None}
+
+yt  = {"small_image":None, "large_image":"yt", "details":"Watching YT",
+	"state":None,"large_text":'Youtube',"small_text":None , "party_size":None,
+	"buttons":[{"label": "Not This", "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}]}
